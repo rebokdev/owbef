64a3510999a3d41f9085b2b072c615f9dca491ea
rebok dev <68105407+rebokdev@users.noreply.github.com>
2022-02-11 16:26:54 +0100

    Add files via upload

---
+++ vmkd/installer.sh
@@ -0,0 +1,15 @@
+cd /home/osmc/
+mkdir features
+cd features
+mkdir vmkd-switcher
+cd vmkd-switcher
+wget https://raw.githubusercontent.com/rebokdev/osmcwebbrowserexperimentalfeautures/patches/vmkd/lxde-rc-on.xml
+https://raw.githubusercontent.com/rebokdev/osmcwebbrowserexperimentalfeautures/patches/vmkd/lxde-rc-off.xml
+wget https://raw.githubusercontent.com/rebokdev/osmcwebbrowserexperimentalfeautures/patches/vmkd/vmkd-switcher.sh
+chmod +x vmkd-switcher.sh
+cd /home/osmc/x11-start/
+mkdir old-vmkd-switcher
+cp vmkd-switcher.sh /home/osmc/x11-start/backup
+wait
+rm vmkd-switcher.sh
+cp /home/osmc/features/vmkd-switcher/vmkd-swittcher.sh /home/osmc/x11-start/
