e954449660baa7f6b5d322ef68e3bcf44b68e1cb
rebok dev <68105407+rebokdev@users.noreply.github.com>
2023-05-30 11:11:49 +0200

    Make vkmd switcher a lot shorter

--- vmkd/vmkd-switcher.sh
+++ vmkd/vmkd-switcher.sh
@@ -1,23 +1,11 @@
 devices=$(lsusb)
 wait
 if echo $devices | grep OSMC; then
-  rm /home/osmc/.config/openbox/lxde-rc.xml
-  wait
-  cp /home/osmc/features/vmkd-switcher/lxde-rc-on.xml /home/osmc/.config/openbox/
-  wait
-  cd /home/osmc/.config/openbox/
-  wait
-  mv lxde-rc-on.xml lxde-rc.xml
+  cp /home/osmc/features/vmkd-switcher/lxde-rc-on.xml /home/osmc/.config/openbox/lxde-rc.xml
   wait
   openbox --reconfigure
 else
-  rm /home/osmc/.config/openbox/lxde-rc.xml
-  wait
-  cp /home/osmc/features/vmkd-switcher/lxde-rc-off.xml /home/osmc/.config/openbox/
-  wait
-  cd /home/osmc/.config/openbox/
-  wait
-  mv lxde-rc-off.xml lxde-rc.xml
+  cp /home/osmc/features/vmkd-switcher/lxde-rc-off.xml /home/osmc/.config/openbox/lxde-rc.xml
   wait
   openbox --reconfigure
 fi
